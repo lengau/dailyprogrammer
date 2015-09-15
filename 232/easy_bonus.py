81c763635a41370ede957094bbb5b6c463e2b5d9
Alex Lowe <amlowe@ieee.org>
2015-09-14 23:03:22 -0500

    Slight improvement to 232 easy bonus.

--- 232/easy_bonus.py
+++ 232/easy_bonus.py
@@ -3,9 +3,22 @@
 # https://redd.it/3kx6oh
 """Two-word palindromes."""
 
+from typing import Union
+
 
 with open('easy/enable1.txt') as dict_file:
     DICTIONARY = dict_file.read().splitlines()
+
+
+def palindrome(a:str, b:str) -> Union[str, None]:
+    """Generates a palindrome from two words if possible.
+
+    If no palindrome exists, returns None."""
+    if a[0] == b[-1] and a[1] == b[-2]:
+        candidate = ''.join((a,b))
+        if candidate == candidate[::-1]:
+            return ' '.join((a,b))
+
 
 def generate_palindromes():
     """Generate two-word palindromes from DICTIONARY.
@@ -15,12 +28,12 @@ def generate_palindromes():
     may not be in alphabetical order."""
     for i in range(len(DICTIONARY)):
         for j in range(i+1, len(DICTIONARY)):
-            candidate = DICTIONARY[i] + DICTIONARY[j]
-            if candidate == candidate[::-1]:
-                yield '%s %s' % (DICTIONARY[i], DICTIONARY[j])
-            candidate = DICTIONARY[j] + DICTIONARY[i]
-            if candidate == candidate[::-1]:
-                yield '%s %s' % (DICTIONARY[j], DICTIONARY[i])
+            candidate = palindrome(DICTIONARY[i], DICTIONARY[j])
+            if candidate:
+                yield candidate
+            candidate = palindrome(DICTIONARY[j], DICTIONARY[i])
+            if candidate:
+                yield candidate
 
 
 if __name__ == '__main__':
