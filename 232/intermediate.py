11f16bc85f796966ce0368d8cee67c1b5bd6e76a
Alex Lowe <amlowe@ieee.org>
2015-09-17 15:10:47 -0500

    Smarter version of 232 intermediate and test data
    I believe 232 intermediate's Neighbourhood.get_closest_smart method has
    run time ~n*logn, although the worst case is still potentially ~n^2.
    
    Added my data (and answers, for those I have it) files.

--- 232/intermediate.py
+++ 232/intermediate.py
@@ -24,7 +24,10 @@ class House(object):
         return sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
 
     def __str__(self) -> str:
-        return '(%s, %s)' % (self.x, self.y)
+        return '(%s,%s)' % (self.x, self.y)
+
+    def __lt__(self, other) -> bool:
+        return self.x < other.x
 
 
 class Neighbourhood(object):
@@ -38,8 +41,23 @@ class Neighbourhood(object):
         shortest_distance = self.houses[-1] @ self.houses[-2]
         closest_houses = (self.houses[-2], self.houses[-1])
         for i in range(len(self.houses) - 2):
-            for j in range(i+1, len(self.houses) - 1):
+            for j in range(i+1, len(self.houses)):
                 distance = self.houses[i] @ self.houses[j]
+                if distance < shortest_distance:
+                    shortest_distance = distance
+                    closest_houses = (self.houses[i], self.houses[j])
+        return closest_houses
+
+    def get_closest_smart(self) -> Tuple[House, House]:
+        """Find the two closest houses using an intelligent algorithm."""
+        self.houses.sort()
+        shortest_distance = self.houses[-1] @ self.houses[-2]
+        closest_houses = (self.houses[-2], self.houses[-1])
+        for i in range(len(self.houses) - 2):
+            for j in range(i+1, len(self.houses)):
+                distance = self.houses[i] @ self.houses[j]
+                if self.houses[j].x - self.houses[i].x >= shortest_distance:
+                    break
                 if distance < shortest_distance:
                     shortest_distance = distance
                     closest_houses = (self.houses[i], self.houses[j])
@@ -62,6 +80,10 @@ class Neighbourhood(object):
 
 def main():
     file = open(sys.argv[1])
+    try:
+        answers = open(sys.argv[2])
+    except IndexError:
+        answers = None
     while True:
         try:
             num_houses = int(file.readline())
@@ -71,9 +93,13 @@ def main():
         for _ in range(num_houses):
             houses.append(file.readline())
         neighbourhood = Neighbourhood.make_neighbourhood(houses)
-        closest = neighbourhood.get_closest()
-
-        print('%s %s' % closest)
+        closest = neighbourhood.get_closest_smart()
+        closest_string = '%s %s' % closest
+        print('Received: %s' % closest_string)
+        if answers is not None:
+            expected = answers.readline().strip()
+            print('Expected: %s' % expected)
+            assert closest_string == expected
     file.close()
 
 if __name__ == '__main__':
